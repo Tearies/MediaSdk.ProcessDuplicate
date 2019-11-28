5a2f6ca4052f63cd97b47faedf7449c3bf6fdd9a
Tearies <xiangmaojun@techstar.com.cn>
2019-11-28 17:46:35 +0800

    添加条件编译

--- MediaSdk.ProcessDuplicate.DwmEx/DwmEx.cpp
+++ MediaSdk.ProcessDuplicate.DwmEx/DwmEx.cpp
@@ -23,9 +23,11 @@ void DwmExManager::CopySource()
 		ID3D11Texture2D* pSharedTexture = nullptr;
 		hr = m_pDevice->OpenSharedResource(targetShardSurface, __uuidof(ID3D11Texture2D), (void**)(&pSharedTexture));
 		m_pDeviceContext->CopyResource(pFrameCopy, pSharedTexture);
+#if DEBUG
 		auto fileName = TEXT("D:\\Render\\") + ::GetTickCount64() + TEXT(".bmp");
 		Util::SaveTextureToBmp(fileName, pFrameCopy);
-		m_pDeviceContext->Unmap(pSharedTexture, 0);
+#endif
+		 
 		pFrameCopy->Release();
 		pSharedTexture->Release();
 	}
